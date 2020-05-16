04c7b8d8e669ced6bb58d9f2176a0b874d406bfa
Nicole Bieger <nicoleregister@email.de>
2020-05-16 16:36:30 +0200

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Bitte gib den ersten Summanden ein:  ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            double ersterSummandZahl = Convert.ToDouble(ersterSummand);
+            double zweiterSummandZahl = Convert.ToDouble(zweiterSummand);
+
+            double summe = ersterSummandZahl + zweiterSummandZahl;
+
+            Console.WriteLine("Die Summe ist {0}", summe);
+
+            Console.ReadLine();
+        }
+    }
+}
