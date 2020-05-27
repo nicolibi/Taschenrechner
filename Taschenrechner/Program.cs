ca961cf3a82e9bdaa782f7233639ccdf8962581f
Nicole Bieger <nicoleregister@email.de>
2020-05-28 00:20:25 +0200

    Auslagerung der Berechnung und Ausgabe in eigene Methode

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -6,41 +6,19 @@ namespace Taschenrechner
     {
         public static void Main()
         {
-            //User Story "Addieren" 
+            //User Storys für Berechnungsoperationen, Abfrage der Oparatoren und Operation
             string ersteZahlalsString = HoleBenutzereingabe("Bitte gib die erste Zahl ein: ");
             string zweiteZahlalsTring = HoleBenutzereingabe("Bitte gib  die zweite Zahl ein: ");
-            string operation = HoleBenutzereingabe("Welche Operation möchtest du ausführen (+ oder -)? ");
+
+            string operation = HoleBenutzereingabe("Welche Operation möchtest du ausführen (+ ,-, /, *)? ");
 
             //Konvertieren in Gleitkommazahl
             //TODO: Auslagern in Funktion wenn der Code umfangreicher wird
             double ersteZahl = Convert.ToDouble(ersteZahlalsString);
             double zweiteZahl = Convert.ToDouble(zweiteZahlalsTring);
 
-            double resultat = 0;
-            switch (operation)
-            {
-                case "+":
-                    resultat = Addiere(ersteZahl, zweiteZahl);
-                    Console.WriteLine("Die Summe ist {0}", resultat);
-                    break;
-
-                case "-":
-                    resultat = Subtrahiere(ersteZahl, zweiteZahl);
-                    Console.WriteLine("Die Differenz ist {0}", resultat);
-                    break;
-
-                case "/":
-                case "*":
-                    Console.WriteLine("Coming Soon");
-                    break;
-
-
-                default:
-                    Console.WriteLine("Du hast eine ungültige Auswahl für die Operation getroffen");
-                    break;
-            }
-
-
+            double resultat = Berechne(ersteZahl, zweiteZahl, operation);
+            GibResultatAus(resultat);
 
             HoleBenutzereingabe("Zum Beenden bitte Enter Taste drücken");
         }
@@ -50,6 +28,38 @@ namespace Taschenrechner
             Console.Write(ausgabetext);
             string summand = Console.ReadLine();
             return summand;
+        }
+
+        static double Berechne(double ersteZahl, double zweiteZahl, string operation)
+        {
+            double resultat = 0;
+            switch (operation)
+            {
+                case "+":
+                    resultat = Addiere(ersteZahl, zweiteZahl);
+                    break;
+
+                case "-":
+                    resultat = Subtrahiere(ersteZahl, zweiteZahl);
+                    break;
+
+                case "/":
+                    resultat = Dividiere(ersteZahl, zweiteZahl);
+                    break;
+
+
+                case "*":
+                    resultat = Multipliziere(ersteZahl, zweiteZahl);
+                    break;
+
+
+                default:
+                    Console.WriteLine("Du hast eine ungültige Auswahl für die Operation getroffen");
+                    break;
+            }
+
+            return resultat;
+
         }
 
         static double Addiere(double ersterSummand, double zweiterSummand)
@@ -64,5 +74,28 @@ namespace Taschenrechner
             double differenz = minunend - subtrahent;
             return differenz;
         }
+
+        static double Multipliziere(double multiplikator, double multiplikant)
+        {
+            double produkt = multiplikator * multiplikant;
+            return produkt;
+        }
+
+        static double Dividiere(double dividend, double divisor)
+        {
+
+            double quotient = dividend / divisor;
+            return quotient;
+        }
+
+        static void GibResultatAus(double resultat)
+        {
+            Console.WriteLine("Das Resultat hat den Wert {0}", resultat);
+        }
+
+
     }
+
+
+
 }
