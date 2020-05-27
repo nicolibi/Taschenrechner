671221770999b060ca95b3fbd268973af07097bf
Nicole Bieger <nicoleregister@email.de>
2020-05-27 23:12:35 +0200

    Implementierung bis Abschnitt 8 (inkl Switch Case fuer Addition und Subtraktion

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -6,18 +6,50 @@ namespace Taschenrechner
     {
         public static void Main()
         {
-            Console.Write("Bitte gib den ersten Summanden ein:  ");
-            string ersterSummand = Console.ReadLine();
-            Console.Write("Bitte gib den zweiten Summanden ein: ");
-            string zweiterSummand = Console.ReadLine();
+            //User Story "Addieren" 
+            string ersteZahlalsString = HoleBenutzereingabe("Bitte gib die erste Zahl ein: ");
+            string zweiteZahlalsTring = HoleBenutzereingabe("Bitte gib  die zweite Zahl ein: ");
+            string operation = HoleBenutzereingabe("Welche Operation möchtest du ausführen (+ oder -)? ");
 
-            double ersterSummandZahl = Convert.ToDouble(ersterSummand);
-            double zweiterSummandZahl = Convert.ToDouble(zweiterSummand);
+            //Konvertieren in Gleitkommazahl
+            //TODO: Auslagern in Funktion wenn der Code umfangreicher wird
+            double ersteZahl = Convert.ToDouble(ersteZahlalsString);
+            double zweiteZahl = Convert.ToDouble(zweiteZahlalsTring);
 
-            double summe = Addiere(ersterSummandZahl, zweiterSummandZahl);
+            double resultat = 0;
+            switch (operation)
+            {
+                case "+":
+                    resultat = Addiere(ersteZahl, zweiteZahl);
+                    Console.WriteLine("Die Summe ist {0}", resultat);
+                    break;
 
-            Console.WriteLine("Die Summe ist {0}", summe);
-            WarteAufBenutzerEingabe();
+                case "-":
+                    resultat = Subtrahiere(ersteZahl, zweiteZahl);
+                    Console.WriteLine("Die Differenz ist {0}", resultat);
+                    break;
+
+                case "/":
+                case "*":
+                    Console.WriteLine("Coming Soon");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Du hast eine ungültige Auswahl für die Operation getroffen");
+                    break;
+            }
+
+
+
+            HoleBenutzereingabe("Zum Beenden bitte Enter Taste drücken");
+        }
+
+        static string HoleBenutzereingabe(string ausgabetext)
+        {
+            Console.Write(ausgabetext);
+            string summand = Console.ReadLine();
+            return summand;
         }
 
         static double Addiere(double ersterSummand, double zweiterSummand)
@@ -25,10 +57,12 @@ namespace Taschenrechner
             double summe = ersterSummand + zweiterSummand;
             return summe;
         }
-        static void WarteAufBenutzerEingabe()
+
+
+        static double Subtrahiere(double minunend, double subtrahent)
         {
-            Console.WriteLine("Zum Beenden bitte Enter Taste drücken");
-            Console.ReadLine();
+            double differenz = minunend - subtrahent;
+            return differenz;
         }
     }
 }
